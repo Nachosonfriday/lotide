e5e7456c32ebd996f71f6199460e3628e5682375
Manuel Zuniga <zuniga@ualberta.ca>
2022-02-07 19:33:29 -0700

    assert function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🤑🤑🤑Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("👹👹👹Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Tonks", "Tonks");
+assertEqual(4,10);
