9e1dcef34b6950bb612bb7582cf5678a5b5d564c
Manuel Zuniga <zuniga@ualberta.ca>
2022-02-07 21:04:52 -0700

    head function test

---
+++ head.js
@@ -0,0 +1,15 @@
+const head = function(array) {
+  return array[0]
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤑🤑🤑Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`👹👹Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,6,7]), 6);
