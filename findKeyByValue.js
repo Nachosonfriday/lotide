fe91ad19586bda527614128a4e601ba9c43ee6c3
Manuel Zuniga <zuniga@ualberta.ca>
2022-02-09 20:33:40 -0700

    final version

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤑🤑🤑Assertion Passed: ${actual} !==  ${expected}`);
+  } else {
+    console.log(`👹👹Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const findKeyByValue = function(newObject, value) {
+  for (const property in newObject) {
+    if (newObject[property] === value) {
+      return property
+    }
+  }
+  return undefined
+}
+
+  //will scan the object for the value and return the first key with that value
+  //if no key value is found, return undefined
+
+  const bestTVShowsByGenre = { 
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+  
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
