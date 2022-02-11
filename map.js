6538a75048da38ba233f3f7be9af5e2eef0c909d
Manuel Zuniga <zuniga@ualberta.ca>
2022-02-10 19:25:54 -0700

    final version of map

---
+++ map.js
@@ -0,0 +1,33 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(words, [ 'g', 'c', 't', 'm', 't' ])
+assertArraysEqual(words, ["ground", "control", "to", "major", "tom"])
