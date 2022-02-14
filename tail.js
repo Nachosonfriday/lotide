76d43ac04eb04d6a5a291713d733e703ec8e7a91
Manuel Zuniga <zuniga@ualberta.ca>
2022-02-14 14:32:56 -0700

    set up test case for tail function

--- tail.js
+++ tail.js
@@ -1,20 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🤑🤑🤑Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`👹👹Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+
+module.exports = tail;
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -12,3 +12,9 @@ console.log("Head Test")
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]), undefined);
+
+console.log("Tail test")
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
