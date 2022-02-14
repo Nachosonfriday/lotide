f5f38d5ea7b6cf7c18221dfc58940471cff198fd
Manuel Zuniga <zuniga@ualberta.ca>
2022-02-14 16:22:04 -0700

    moved the assert arrays equal test info to the assert equal test file

--- head.js
+++ head.js
@@ -8,13 +8,7 @@ const head = function(array) {
   
 }
 
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`🤑🤑🤑Assertion Passed: ${actual} ===  ${expected}`);
-//   } else {
-//     console.log(`👹👹Assertion Failed: ${actual} !==  ${expected}`);
-//   }
-// };
+
 
 module.exports = head;
 // assertEqual(head([5,6,7]), 5);
