42a73d1df4ad2c98d6a1d6838eb0e74cb03702c4
Manuel Zuniga <zuniga@ualberta.ca>
2022-02-14 18:20:34 -0700

    added on chai and mocha testing to tail test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-console.log("Tail test")
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    const arr = ["Hello", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(arr), ["Lighthouse", "Labs"]);
+  });
+
+  it('returns [1,2,3,4,5] for [0,1,2,3,4,5]', () => {
+    const arr = [0,1,2,3,4,5]
+    assert.deepEqual(tail(arr), [1,2,3,4,5]);
+  });
+});
